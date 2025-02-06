eb0b1beb0a2a49109b4975df577c9a7c522f28e1
wkdus0608 <wkdus0608@naver.com>
2025-02-06 15:38:18 +0900

    routes 및 controller로 코드 관리

---
+++ firstProject/backend/routes/product.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+import { getProducts, updateProduct, createProduct, deleteProduct } from "../controllers/product.controller.js";
+
+const router = express.Router();
+
+router.get("/", getProducts);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
+
+export default router;
--- firstProject/backend/server.js
+++ firstProject/backend/server.js
@@ -2,8 +2,8 @@ import express from "express";
 // dotenv = 민감한 정보 관리하는 Node.js 패키지
 import dotenv from 'dotenv';
 import connectDB from "./config/db.js";
-import Product from "./models/product.model.js"; // 모델 import
-import mongoose from "mongoose";
+import productRoutes from "./routes/product.route.js"
+
 
 dotenv.config();
 connectDB();
@@ -12,70 +12,9 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/api/products", async (req, res) => {
-	try {
-		const products = await Product.find({});
-		res.status(200).json({ success: true, data: products });
-	} catch (error) {
-		console.log("error in fetching products:", error.message);
-		res.status(500).json({ success: false, message: "Server Error" });
-	}
-})
-
-app.post("/api/products", async (req, res) => {
-	const product = req.body; // user will send this data
-
-	if (!product.name || !product.price || !product.image) {
-		return res.status(400).json({ success: false, message: "Please provide all fields" });
-	}
-
-	const newProduct = new Product(product);
-
-	try {
-		await newProduct.save();
-		res.status(201).json({ success: true, data: newProduct });
-	} catch (error) {
-		console.error("Error in Create product:", error.message);
-		res.status(500).json({ success: false, message: "Server Error" });
-	}
-});
-
-app.put("/api/products/:id", async (req, res) => {
-	const { id } = req.params;
-
-	const product = req.body;
-
-	if (!mongoose.Types.ObjectId.isValid(id)) {
-		return res.status(404).json({ success: false, message: "Invalid Product Id" });
-	}
-
-	try {
-		const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
-		res.status(200).json({ success: true, data: updatedProduct });
-	} catch (error) {
-		res.status(500).json({ success: false, message: "Server Error" });
-	}
-});
-
-
-app.delete("/api/products/:id", async (req, res) => {
-	const { id } = req.params;
-
-	if (!mongoose.Types.ObjectId.isValid(id)) {
-		return res.status(404).json({ success: false, message: "Invalid Product Id" });
-	}
-
-	try {
-		await Product.findByIdAndDelete(id);
-		res.status(200).json({ success: true, message: "Product deleted" });
-	} catch (error) {
-		console.log("error in deleting product:", error.message);
-		res.status(500).json({ success: false, message: "Server Error" });
-	}
-
-});
+app.use("/api/products", productRoutes);
 
 // 5000은 점유중인 상태 -> 5001로 변경.
 app.listen(5001, () => {
-    console.log("Server started at http://localhost:5001");
+	console.log("Server started at http://localhost:5001");
 });
