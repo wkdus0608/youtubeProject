fcaa623044ad1f5d3108a314f0262b6cd60940b9
wkdus0608 <wkdus0608@naver.com>
2025-02-07 17:29:16 +0900

    다시 해볼까?

--- firstProject/frontend/src/components/Navbar.jsx
+++ firstProject/frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Container } from '@chakra-ui/react'
 import React from 'react'
 
 const  Navbar = () => {
-  return <Container></Container>
+  return <Container>NavBar</Container>
 }
 
 export default Navbar
