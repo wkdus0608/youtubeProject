e1c72061a738ddc62a98d73f499014212227829a
wkdus0608 <wkdus0608@naver.com>
2025-02-08 13:36:44 +0900

    Product Store 까지 뜸

--- firstProject/frontend/src/components/Navbar.jsx
+++ firstProject/frontend/src/components/Navbar.jsx
@@ -1,8 +1,33 @@
-import { Container } from '@chakra-ui/react'
-import React from 'react'
+import { Container, Flex, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Link } from '@chakra-ui/react';
 
-const  Navbar = () => {
-  return <Container>NavBar</Container>
-}
+const Navbar = () => {
+  return (
+    <Container maxW={'1140px'} px={4}>
+      <Flex
+        h={16}
+        alignItems={'center'}
+        justifyContent={'space-between'}
+        flexDir={{
+          base: 'column',
+          sm: 'row',
+        }}
+      >
+        <Text
+          fontSize={{ base: '22', sm: '28' }}
+          fontWeight={'bold'}
+          textTransform={'uppercase'}
+          textAlign={'center'}
+          bgGradient={'linear(to-r, cyan.400, blue.500)'}
+          bgClip={'text'}
+        >
+          <Link to={'/'}>Product Store 🛒</Link>
+        </Text>
+      </Flex>
+      NavBar
+    </Container>
+  );
+};
 
-export default Navbar
+export default Navbar;
