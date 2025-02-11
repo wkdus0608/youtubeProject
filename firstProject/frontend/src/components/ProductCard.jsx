6da000e97c935618453ebb7adc60c5fc37289f71
wkdus0608 <seongmin020608@gmail.com>
2025-02-11 15:29:57 +0900

    상품 삭제

--- firstProject/frontend/src/components/ProductCard.jsx
+++ firstProject/frontend/src/components/ProductCard.jsx
@@ -6,13 +6,39 @@ import {
   HStack,
   IconButton,
   Heading,
+  useToast,
+  useColorModeValue,
 } from '@chakra-ui/react';
 import { DeleteIcon, EditIcon } from '@chakra-ui/icons';
-import { useColorModeValue } from './ui/color-mode';
+// import {  } from './ui/color-mode';
+import { useProductStore } from './store/product';
 
 const ProductCard = ({ product }) => {
   const textColor = useColorModeValue('gray.600', 'gray.200');
   const bg = useColorModeValue('white', 'gray.800');
+
+  const { deleteProduct } = useProductStore();
+  const toast = useToast();
+
+  const handleDeleteProduct = async (pid) => {
+    const { success, message } = await deleteProduct(pid);
+    if (!success) {
+      toast({
+        title: 'Error',
+        description: message,
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+    } else
+      toast({
+        title: 'Success',
+        description: message,
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
+      });
+  };
 
   return (
     <Box
@@ -42,7 +68,11 @@ const ProductCard = ({ product }) => {
 
         <HStack spacing={2}>
           <IconButton icon={<EditIcon />} colorScheme='blue' />
-          <IconButton icon={<DeleteIcon />} colorScheme='red' />
+          <IconButton
+            icon={<DeleteIcon />}
+            onClick={() => handleDeleteProduct(product._id)}
+            colorScheme='red'
+          />
         </HStack>
       </Box>
     </Box>
--- firstProject/frontend/src/components/store/product.js
+++ firstProject/frontend/src/components/store/product.js
@@ -1,44 +1,55 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 export const useProductStore = create((set) => ({
-  products: [],
-  setProducts: (products) => set({ products }),
-  createProduct: async (newProduct) => {
-    try {
-      if (!newProduct.name || !newProduct.image || !newProduct.price) {
-        return { success: false, message: 'Please fill in all fields.' };
-      }
+	products: [],
+	setProducts: (products) => set({ products }),
+	createProduct: async (newProduct) => {
+		if (!newProduct.name || !newProduct.image || !newProduct.price) {
+			return { success: false, message: "Please fill in all fields." };
+		}
+		const res = await fetch("/api/products", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(newProduct),
+		});
+		const data = await res.json();
+		set((state) => ({ products: [...state.products, data.data] }));
+		return { success: true, message: "Product created successfully" };
+	},
+	fetchProducts: async () => {
+		const res = await fetch("/api/products");
+		const data = await res.json();
+		set({ products: data.data });
+	},
+	deleteProduct: async (pid) => {
+		const res = await fetch(`/api/products/${pid}`, {
+			method: "DELETE",
+		});
+		const data = await res.json();
+		if (!data.success) return { success: false, message: data.message };
 
-      // const res = await fetch('http://localhost:5000/api/products', {
-      //   method: 'POST',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //   },
-      //   body: JSON.stringify(newProduct),
-      // });
-      // product.js
-      const res = await fetch('/api/products', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newProduct),
-      });
+		// update the ui immediately, without needing a refresh
+		set((state) => ({ products: state.products.filter((product) => product._id !== pid) }));
+		return { success: true, message: data.message };
+	},
+	// updateProduct: async (pid, updatedProduct) => {
+	// 	const res = await fetch(`/api/products/${pid}`, {
+	// 		method: "PUT",
+	// 		headers: {
+	// 			"Content-Type": "application/json",
+	// 		},
+	// 		body: JSON.stringify(updatedProduct),
+	// 	});
+	// 	const data = await res.json();
+	// 	if (!data.success) return { success: false, message: data.message };
 
-      const data = await res.json();
-      set((state) => ({ products: [...state.products, data.data] }));
-      return { success: true, message: 'Product created successfully' };
-    } catch (error) {
-      console.error('Error creating product:', error);
-      return {
-        success: false,
-        message: error.message || 'Failed to create product',
-      };
-    }
-  },
-  fetchProducts: async () => {
-    const res = await fetch("/api/products");
-    const data = await res.json();
-    set({ products: data.data });
-  }
+	// 	// update the ui immediately, without needing a refresh
+	// 	set((state) => ({
+	// 		products: state.products.map((product) => (product._id === pid ? data.data : product)),
+	// 	}));
+
+	// 	return { success: true, message: data.message };
+	// },
 }));
